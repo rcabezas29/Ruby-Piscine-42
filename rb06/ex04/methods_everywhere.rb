281df24b9442a4891df97ab97b53a917fd93ad14
Raul Cabezas gonzalez <rcabezas@c1r2s5.42madrid.com>
2021-06-08 09:10:53 +0200

    Finish rb06

--- rb06/ex04/methods_everywhere.rb
+++ rb06/ex04/methods_everywhere.rb
@@ -1,2 +1,23 @@
-#!/usr/bin/env ruby
+#/usr/bin/env ruby
 
+def reduce(str)
+	str[0..7]
+end
+
+def agranda(str)
+	str.ljust(8, 'Z')
+end
+	
+args = ARGV
+if args.length == 0
+	puts 'none'
+end
+for s in args do
+	if s.length > 8
+		puts reduce(s)
+	elsif s.length < 8
+		puts agranda(s)
+	else
+		puts s
+	end
+end
---
+++ rb06/ex05/scope_that.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def add_one(n)
+    n + 1
+end
+
+n = 7
+puts n
+add_one(n)
+puts n
+n = add_one(n)
+puts n
